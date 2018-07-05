a903748ebc16fec8a23a7e19f5ecb66355be1333
Egoitz Cespedes Aristu <ecespedes@cistec.local>
2018-07-05 10:19:45 +0200

    Input, Output y viewchild

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -3,6 +3,7 @@ import { PagesComponent } from './pages.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { Graficas1Component } from './graficas1/graficas1.component';
 import { ProgressComponent } from './progress/progress.component';
+import { FormsModule } from '@angular/forms';
 
 // Modulos
 import { SharedModule } from '../shared/shared.module';
@@ -10,21 +11,34 @@ import { SharedModule } from '../shared/shared.module';
 // Rutas
 import { PAGES_ROUTES } from './pages.routes';
 
+// Temporal
+import { IncrementadorComponent } from '../components/incrementador/incrementador.component';
+
+// Graficas
+import { GraficasComponent } from '../components/graficas/graficas.component';
+import { ChartsModule } from 'ng2-charts';
+
 @NgModule({
     declarations: [
         PagesComponent,
         DashboardComponent,
         ProgressComponent,
-        Graficas1Component
+        Graficas1Component,
+        IncrementadorComponent,
+        GraficasComponent
     ],
     exports: [
         DashboardComponent,
         ProgressComponent,
-        Graficas1Component
+        Graficas1Component,
+        IncrementadorComponent,
+        GraficasComponent
     ],
     imports: [
         SharedModule,
-        PAGES_ROUTES
+        PAGES_ROUTES,
+        FormsModule,
+        ChartsModule
     ]
 })
 
